51d39907b148a488bf3e22d421e28007c55522a2
HolohotsIhor <gologocigor@gmail.com>
2022-08-18 13:03:01 +0300

    Done

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,25 @@
-$(document).ready(function() {
-  console.log("Script has been loaded!");
-}); 
+(function (d) {
+    const btnForm = d.querySelector("#btnSubmit");
+    const emailSelector = "#email";
+    const passwordSelector = "#password";
+    const email = d.querySelector(emailSelector);
+    const password = d.querySelector(passwordSelector);
+
+    const isFieldEmpty = (value, selector) => {
+        if (!value) {
+            d.querySelector(selector).parentNode.classList.add("error");
+        } else {
+            d.querySelector(selector).parentNode.classList.remove("error");
+        }
+    }
+
+    const validation = () => {
+        const emailText = email.value;
+        const passwordText = password.value;
+
+        isFieldEmpty(emailText, emailSelector);
+        isFieldEmpty(passwordText, passwordSelector);
+    }
+
+    btnForm.addEventListener("click",validation);
+}(document));
